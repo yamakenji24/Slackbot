0f5a7b10c45961b00480d007e1dc47429a133e2c
Kenji Yamashita <s16t278@stu.kagawa-u.ac.jp>
2019-04-11 23:54:18 +0900

    add weather info

--- slack.rb
+++ slack.rb
@@ -1,10 +1,21 @@
+# coding: utf-8
 require 'slack-ruby-client'
+require 'json'
+require 'open-uri'
+
+uri = "http://weather.livedoor.com/forecast/webservice/json/v1?city=370000"
 
 TOKEN = ''
 
 Slack.configure do |conf|
   conf.token = TOKEN
 end
+
+res = JSON.load(open(uri).read)
+title = res['title']
+link = res['link']
+weather = res['forecasts'].first
+message = "[#{weather['date']}の#{title}]は「#{weather['telop']}」です。"
 
 client = Slack::RealTime::Client.new
 
@@ -16,6 +27,8 @@ end
 client.on :message do |data|
   if data['text'].include?('hi')
     client.message channel: data['channel'], text: "Hi!!!"
+  elsif data['text'].include?('weather')
+    client.message channel: data['channel'], text: message
   else
     client.message channel: data['channel'], text: "Thank you"
   end
